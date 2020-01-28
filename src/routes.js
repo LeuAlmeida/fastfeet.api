3bf693c6c53f5260b994277b3a68c93cfe233f17
LeuAlmeida <leo@webid.net.br>
2020-01-28 15:02:44 -0300

    feat: Improving multer settings into routes and create Deliveryman store method

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/login', SessionController.store);
 
@@ -15,5 +19,7 @@ routes.post('/recipients', RecipientController.store);
 routes.get('/recipients', RecipientController.index);
 routes.put('/recipients/:id', RecipientController.update);
 routes.delete('/recipients/:id', RecipientController.delete);
+
+routes.post('/deliveryman', DeliverymanController.store);
 
 export default routes;
