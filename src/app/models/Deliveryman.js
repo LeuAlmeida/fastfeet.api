818ec4738bc0229909ba9fd971c90695669c8ccf
LeuAlmeida <leo@webid.net.br>
2020-01-28 16:37:31 -0300

    fix: Rename Deliveryman model

--- src/app/models/Deliveryman.js
+++ src/app/models/Deliveryman.js
@@ -1,6 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 
-class Recipient extends Model {
+class Deliveryman extends Model {
   static init(sequelize) {
     super.init(
       {
@@ -20,4 +20,4 @@ class Recipient extends Model {
   // }
 }
 
-export default Recipient;
+export default Deliveryman;
