57f98bad06b11ce69e124e130a24e3ed8d7b0ec3
LeuAlmeida <leo@webid.net.br>
2020-01-28 17:43:05 -0300

    fix: Rename deliveryman reference

--- src/database/migrations/20200128203325-create-delivery.js
+++ src/database/migrations/20200128203325-create-delivery.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true,
         references: {
-          model: 'deliveryman',
+          model: 'deliverymans',
           key: 'id',
         },
         onUpdate: 'CASCADE',
