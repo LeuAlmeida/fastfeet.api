75d393e90a6080418f966ce40f06fa46c662b105
LeuAlmeida <leo@webid.net.br>
2020-01-28 18:18:35 -0300

    feat: Improving update method to users

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,7 +16,9 @@ class UserController {
         .email()
         .required(),
       name: Yup.string().required(),
-      password: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
     });
 
     if (!(await schema.isValid(req.body))) {
@@ -47,7 +49,59 @@ class UserController {
   }
 
   async update(req, res) {
-    return null;
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(5),
+      password: Yup.string()
+        .min(5)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { id } = req.params;
+
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return res.status(400).json({ error: 'User does not found.' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    if (email !== user.email) {
+      const emailExists = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (emailExists) {
+        return res
+          .status(401)
+          .json({ error: 'An user with this email already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match. ' });
+    }
+
+    const { name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
