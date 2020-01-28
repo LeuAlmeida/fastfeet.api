6be047a2da08553c05eb60c245240c8b84955961
LeuAlmeida <leo@webid.net.br>
2020-01-28 13:35:22 -0300

    fix: Rename Recipient model

--- src/app/models/Recipient.js
+++ src/app/models/Recipient.js
@@ -1,6 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 
-class User extends Model {
+class Recipient extends Model {
   static init(sequelize) {
     super.init(
       {
@@ -21,4 +21,4 @@ class User extends Model {
   }
 }
 
-export default User;
+export default Recipient;
