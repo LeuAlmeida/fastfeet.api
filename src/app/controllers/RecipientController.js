95b6fc616323b3e1a5b534c5fc3946593301fae9
LeuAlmeida <leo@webid.net.br>
2020-01-28 13:35:04 -0300

    feat: Incrementing Recipient controller with store method

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,8 +1,27 @@
-// import Recipient from '../models/Recipient';
+import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
-    return res.json({ ok: true });
+    const { name, address, number, state, city, cep } = req.body;
+
+    const recipientExists = await Recipient.findOne({
+      where: { name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists.' });
+    }
+
+    await Recipient.create(req.body);
+
+    return res.json({
+      name,
+      address,
+      number,
+      state,
+      city,
+      cep,
+    });
   }
 }
 
