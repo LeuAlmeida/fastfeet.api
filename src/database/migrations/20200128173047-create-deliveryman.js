4f72fe77ed19a9c8bc929b47a96701677b49cb68
LeuAlmeida <leo@webid.net.br>
2020-01-28 16:37:54 -0300

    feat: Improving deliveryman migration

--- src/database/migrations/20200128173047-create-deliveryman.js
+++ src/database/migrations/20200128173047-create-deliveryman.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('deliveryman', {
+    return queryInterface.createTable('deliverymans', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
