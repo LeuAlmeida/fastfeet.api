9c5c97a80d892415c4cb579432c8bd6334f286f7
LeuAlmeida <leo@webid.net.br>
2020-01-28 11:57:54 -0300

    feat: Initial database settings

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: '',
+  host: '',
+  username: '',
+  password: '',
+  database: '',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
