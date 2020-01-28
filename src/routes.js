45349130db76a6c034db79dcf4c2204815dbebe6
LeuAlmeida <leo@webid.net.br>
2020-01-28 16:36:35 -0300

    feat: Incrementing files upload route

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,6 +15,8 @@ const upload = multer(multerConfig);
 routes.post('/login', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/recipients', RecipientController.store);
 routes.get('/recipients', RecipientController.index);
