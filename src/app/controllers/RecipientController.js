49f214629eb6f2b395cd845ef9febf93771c5ef9
LeuAlmeida <leo@webid.net.br>
2020-01-28 13:50:20 -0300

    feat: Improving Recipient Controller with updat method (finished)

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -63,6 +63,59 @@ class RecipientController {
 
     return res.json(recipients);
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      address: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    const recipient = await Recipient.findOne({
+      where: { id },
+    });
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not found.' });
+    }
+
+    const { name } = req.body;
+
+    if (name !== recipient.name) {
+      const recipientExists = await Recipient.findOne({
+        where: { name },
+      });
+
+      if (recipientExists) {
+        return res
+          .status(401)
+          .json({ error: 'A recipient with this name already exists.' });
+      }
+    }
+
+    const { address, number, state, city, cep } = await recipient.update(
+      req.body
+    );
+
+    return res.json({
+      name,
+      address,
+      number,
+      state,
+      city,
+      cep,
+    });
+  }
 }
 
 export default new RecipientController();
