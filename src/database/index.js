85cd30b73fc1b17745dfa071e8e4614e750e10a2
LeuAlmeida <leo@webid.net.br>
2020-01-28 14:53:38 -0300

    feat: Include Deliveryman into database index

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import Deliveryman from '../app/models/Deliveryman';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient];
+const models = [User, Recipient, Deliveryman];
 
 class Database {
   constructor() {
