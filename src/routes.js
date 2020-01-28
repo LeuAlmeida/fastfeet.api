b2204fe05983afa66b9755242071c10248959fcc
LeuAlmeida <leo@webid.net.br>
2020-01-28 13:22:31 -0300

    feat: Introducing Recipient routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -9,5 +10,7 @@ const routes = new Router();
 routes.post('/login', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
