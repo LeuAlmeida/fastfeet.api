bcc342519dc1b222155781224b87719ffe339824
LeuAlmeida <leo@webid.net.br>
2020-01-28 18:00:11 -0300

    feat: Introducing CRUD to users

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,21 @@
+import User from '../models/User';
+
+class UserController {
+  async index(req, res) {
+    return null;
+  }
+
+  async store(req, res) {
+    return null;
+  }
+
+  async delete(req, res) {
+    return null;
+  }
+
+  async update(req, res) {
+    return null;
+  }
+}
+
+export default UserController();
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
 import FileController from './app/controllers/FileController';
@@ -17,6 +18,11 @@ routes.post('/login', SessionController.store);
 routes.use(authMiddleware);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/users', UserController.store);
+routes.get('/users', UserController.index);
+routes.put('/users', UserController.update);
+routes.delete('/users', UserController.delete);
 
 routes.post('/recipients', RecipientController.store);
 routes.get('/recipients', RecipientController.index);
