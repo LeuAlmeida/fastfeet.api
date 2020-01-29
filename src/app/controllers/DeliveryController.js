ee2ae0baf0c41983f16c60c10f33395e22f47627
LeuAlmeida <leo@webid.net.br>
2020-01-29 15:00:27 -0300

    feat: Incrementing post method to Delivery controller

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -1,4 +1,9 @@
-// import Delivery from '../models/Delivery';
+import * as Yup from 'yup';
+
+import Delivery from '../models/Delivery';
+import Recipient from '../models/Recipient';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
 
 class DeliveryController {
   async index(req, res) {
@@ -6,7 +11,52 @@ class DeliveryController {
   }
 
   async store(req, res) {
-    return null;
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number().required(),
+      deliveryman_id: Yup.number().required(),
+      signature_id: Yup.number().required(),
+      product: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { recipient_id, deliveryman_id, signature_id } = req.body;
+
+    const recipient = await Recipient.findOne({
+      where: {
+        id: recipient_id,
+      },
+    });
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not found.' });
+    }
+
+    const deliveryman = await Deliveryman.findOne({
+      where: {
+        id: deliveryman_id,
+      },
+    });
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman does not found.' });
+    }
+
+    const signature = await File.findOne({
+      where: {
+        id: signature_id,
+      },
+    });
+
+    if (!signature) {
+      return res.status(400).json({ error: 'Signature does not found.' });
+    }
+
+    const delivery = await Delivery.create(req.body);
+
+    return res.json(delivery);
   }
 
   async update(req, res) {
