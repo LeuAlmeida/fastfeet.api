393ff033b5647f06e7c8504159e2c75cbb6007a1
LeuAlmeida <leo@webid.net.br>
2020-01-29 15:19:06 -0300

    feat: Introducing update method and improving POST

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -60,6 +60,11 @@ class DeliveryController {
     }
 
     const { recipient_id, deliveryman_id, signature_id } = req.body;
+    let { start_date } = req.body;
+
+    if (!start_date) {
+      start_date = new Date();
+    }
 
     const recipient = await Recipient.findOne({
       where: {
@@ -91,13 +96,97 @@ class DeliveryController {
       return res.status(400).json({ error: 'Signature does not found.' });
     }
 
-    const delivery = await Delivery.create(req.body);
+    const delivery = await Delivery.create({ ...req.body, start_date });
 
     return res.json(delivery);
   }
 
   async update(req, res) {
-    return null;
+    const schema = Yup.object().shape({
+      product: Yup.string(),
+      recipient_id: Yup.number(),
+      deliveryman_id: Yup.number(),
+      signature_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { recipient_id, deliveryman_id, signature_id } = req.body;
+
+    const recipient = await Recipient.findOne({
+      where: {
+        id: recipient_id,
+      },
+    });
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not found.' });
+    }
+
+    const deliveryman = await Deliveryman.findOne({
+      where: {
+        id: deliveryman_id,
+      },
+    });
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman does not found.' });
+    }
+
+    const signature = await File.findOne({
+      where: {
+        id: signature_id,
+      },
+    });
+
+    if (!signature) {
+      return res.status(400).json({ error: 'Signature does not found.' });
+    }
+
+    const { id } = req.params;
+
+    const delivery = await Delivery.findByPk(id);
+
+    if (!delivery) {
+      return res.status(400).json({ error: 'Delivery does not found' });
+    }
+
+    await delivery.update(req.body);
+
+    const { start_date, end_date, canceled_at } = delivery;
+
+    const { product } = await Delivery.findByPk(id, {
+      include: [
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: ['id', 'name'],
+        },
+        {
+          model: Deliveryman,
+          as: 'deliveryman',
+          attributes: ['id', 'name', 'email'],
+        },
+        {
+          model: File,
+          as: 'signature',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json({
+      id,
+      product,
+      start_date,
+      end_date,
+      canceled_at,
+      recipient,
+      deliveryman,
+      signature,
+    });
   }
 
   async delete(req, res) {
