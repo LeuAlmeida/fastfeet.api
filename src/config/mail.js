cedea79cbcb88faa183e613f0c8d0116d3954af5
LeuAlmeida <leo@webid.net.br>
2020-01-29 19:17:01 -0300

    env: Improving mail environments configs

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: '',
-  port: '',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '',
-    pass: '',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Fastfeet <noreply@fastfeet.com.br>',
