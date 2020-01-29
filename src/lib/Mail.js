4624e1aa73d0fd956dcf081b8033eab0939339ec
LeuAlmeida <leo@webid.net.br>
2020-01-29 19:20:54 -0300

    feat: Introducing node handlebars into Mail lib file

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,3 +1,6 @@
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
 import nodemailer from 'nodemailer';
 import mailConfig from '../config/mail';
 
@@ -11,6 +14,26 @@ class Mail {
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendMail(message) {
