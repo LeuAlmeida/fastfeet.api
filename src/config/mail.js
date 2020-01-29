5db99fe04868bedac42ddc4fd890387f09368fe8
LeuAlmeida <leo@webid.net.br>
2020-01-29 19:13:22 -0300

    feat: Improving mail settings file

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: '',
+  port: '',
+  secure: false,
+  auth: {
+    user: '',
+    pass: '',
+  },
+  default: {
+    from: 'Equipe Fastfeet <noreply@fastfeet.com.br>',
+  },
+};
