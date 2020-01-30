177b042c0fadd96e8c25d19691f6302724b6f2a9
LeuAlmeida <leo@webid.net.br>
2020-01-30 16:43:54 -0300

    chore: Remove signature from delivery controller

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -55,17 +55,17 @@ class DeliveryController {
 
   async store(req, res) {
     const schema = Yup.object().shape({
+      product: Yup.string().required(),
       recipient_id: Yup.number().required(),
       deliveryman_id: Yup.number().required(),
-      signature_id: Yup.number().required(),
-      product: Yup.string().required(),
+      signature_id: Yup.number(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails.' });
     }
 
-    const { product, recipient_id, deliveryman_id, signature_id } = req.body;
+    const { product, recipient_id, deliveryman_id } = req.body;
 
     const recipient = await Recipient.findOne({
       where: {
@@ -87,19 +87,9 @@ class DeliveryController {
       return res.status(400).json({ error: 'Deliveryman does not found.' });
     }
 
-    const signature = await File.findOne({
-      where: {
-        id: signature_id,
-      },
-    });
-
-    if (!signature) {
-      return res.status(400).json({ error: 'Signature does not found.' });
-    }
-
     let delivery;
 
-    if (recipient && deliveryman && signature) {
+    if (recipient && deliveryman) {
       delivery = await Delivery.create(req.body);
 
       await Queue.add(DeliveryConfirmationMail.key, {
