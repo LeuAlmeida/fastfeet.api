103bc73c316264d87ca089538769fb93331be02a
LeuAlmeida <leo@webid.net.br>
2020-01-30 19:17:48 -0300

    feat: Introducing DeliveryProblem into database index

--- src/database/index.js
+++ src/database/index.js
@@ -4,11 +4,12 @@ import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import Deliveryman from '../app/models/Deliveryman';
 import Delivery from '../app/models/Delivery';
+import DeliveryProblem from '../app/models/DeliveryProblem';
 import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, Deliveryman, Delivery, File];
+const models = [User, Recipient, Deliveryman, Delivery, DeliveryProblem, File];
 
 class Database {
   constructor() {
