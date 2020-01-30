721cfb604497ae6f1fcdf6a73687f1e78b57b34d
LeuAlmeida <leo@webid.net.br>
2020-01-30 13:40:22 -0300

    feat: Implementing deliveries per deliveryman (list)

--- src/app/controllers/DeliveriesPerDeliverymanController.js
+++ src/app/controllers/DeliveriesPerDeliverymanController.js
@@ -1,9 +1,29 @@
-// import Delivery from '../models/Delivery';
-// import Deliveryman from '../models/Deliveryman';
+import Delivery from '../models/Delivery';
+import Deliveryman from '../models/Deliveryman';
 
 class DeliveriesPerDeliverymanController {
   async index(req, res) {
-    return res.json({ ok: true });
+    const { id } = req.params;
+
+    const deliveryman = await Deliveryman.findByPk(id);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman does not found. ' });
+    }
+
+    const { page = 1 } = req.query;
+
+    const deliveries = await Delivery.findAll({
+      where: {
+        deliveryman_id: id,
+        canceled_at: null,
+      },
+      attributes: ['id', 'product', 'start_date', 'end_date', 'recipient_id'],
+      limit: 10,
+      offset: (page - 1) * 10,
+    });
+
+    return res.json(deliveries);
   }
 }
 
