73ce564b1ed4334befb68cdedd857a7bc15b2a4d
LeuAlmeida <leo@webid.net.br>
2020-02-19 11:20:16 -0300

    chore(seed): remove signature seed

--- src/database/seeds/20200219140230-file.js
+++ src/database/seeds/20200219140230-file.js
@@ -10,13 +10,13 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date(),
         },
-        {
-          id: 2,
-          name: 'signature.png',
-          path: '5aff4473187184f82bccb87ef39d48eb.png',
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
+        // {
+        //   id: 2,
+        //   name: 'signature.png',
+        //   path: '5aff4473187184f82bccb87ef39d48eb.png',
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
       ],
       {}
     );
