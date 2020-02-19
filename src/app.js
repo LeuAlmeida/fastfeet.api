2e7ab1952c82f1262b42966ff9c0359344ad9c97
LeuAlmeida <leo@webid.net.br>
2020-02-19 10:43:52 -0300

    feat(app): improvements and features of failure handlebars

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -10,11 +14,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -24,6 +32,18 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.requestHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal server error.' });
+    });
   }
 }
 
