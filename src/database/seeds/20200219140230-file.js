e20c212ba60396230a87dd7a11fa2c65670cb046
LeuAlmeida <leo@webid.net.br>
2020-02-19 11:09:03 -0300

    feat(seed): update file seeder with avatar img

--- src/database/seeds/20200219140230-file.js
+++ src/database/seeds/20200219140230-file.js
@@ -5,6 +5,13 @@ module.exports = {
       [
         {
           id: 1,
+          name: 'profile.jpeg',
+          path: '6cd077a78f748f49a5e91c71f526dc24.jpeg',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 2,
           name: 'signature.png',
           path: '5aff4473187184f82bccb87ef39d48eb.png',
           created_at: new Date(),
