09810dae7cd74b5523ea00d0771e747b82013f7c
LeuAlmeida <leo@webid.net.br>
2020-03-04 18:59:11 -0300

    feat(migration): introducing Status column into Delivery table

--- src/database/migrations/20200128203325-create-delivery.js
+++ src/database/migrations/20200128203325-create-delivery.js
@@ -53,6 +53,10 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: true,
       },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
