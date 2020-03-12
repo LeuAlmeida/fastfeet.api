347bd04552d6cf8f703b53aea3585f134de18ac3
LeuAlmeida <leo@webid.net.br>
2020-03-12 19:02:45 -0300

    fix(migration): resolve integer conflicts

--- src/database/migrations/20200128161418-create-recipients.js
+++ src/database/migrations/20200128161418-create-recipients.js
@@ -33,7 +33,7 @@ module.exports = {
         allowNull: false,
       },
       cep: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         allowNull: false,
       },
       created_at: {
